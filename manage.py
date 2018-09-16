bc480ea830605b1503e2b498766d2f065c4c65f6
Levy <leskeylevy@gmail.com>
2018-09-16 18:00:21 +0300

    deploying to heroku - 3

--- manage.py
+++ manage.py
@@ -1,13 +1,13 @@
 from flask_script import Manager, Server
 from app import create_app, db
-from app.models import User
-from flask_migrate import Migrate,MigrateCommand
+from app.models import User,Pitch
+from flask_migrate import Migrate, MigrateCommand
 
 app = create_app('production')
 
 manager = Manager(app)
-migrate=Migrate(app,db)
-manager.add_command('db',MigrateCommand)
+migrate = Migrate(app,db)
+manager.add_command('db', MigrateCommand)
 manager.add_command('server', Server)
 
 
@@ -16,6 +16,5 @@ def make_shell_context():
     return dict(app=app, db=db, User=User)
 
 
-if __name__ == '__main__'
-    SECRET_KEY=123456789
+if __name__ == '__main__':
     manager.run()
