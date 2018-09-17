9fa362794f2f142d2a08cdfeec572831012d38fb
Levy <leskeylevy@gmail.com>
2018-09-17 13:14:21 +0300

    deploying to heroku

--- config.py
+++ config.py
@@ -2,12 +2,18 @@ import os
 
 
 class Config:
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     SECRET_KEY = os.environ.get('SECRET_KEY')
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://levy:newpassword@localhost/blog'
 
 
 class ProdConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     pass
 
 class DevConfig(Config):
--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
 export SECRET_KEY=123456789
+export MAIL_USERNAME='slimleskey@gmail.com'
+export MAIL_PASSWORD='1234$56879L'
+export DATABASE_URL='postgres://flnriigvthlkuc:2737cd55d03c954d8b1816de7c82872c849d297993ea029e9cae8fc4f3cf9072@ec2-54-83-4-76.compute-1.amazonaws.com:5432/d7uuptgoqemov1'
 python3.6 manage.py server
